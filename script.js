5098bf7a761e9511cc9675d1df5dc281afda8e58
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-06-26 19:14:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let button = document.getElementsByTagName('input')[0];
+button.addEventListener('click',func);
+
+function func() {
+	let selectmainelem=document.getElementById('colorSelect');
+	let selectedelem = selectmainelem.options[selectmainelem.selectedIndex];
+	console.log(selectedelem);
+   selectmainelem.removeChild(selectedelem);
+}
